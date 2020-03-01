cc80d7fbfb624e76de528cdcf6f454cab2d96b45
R,Divakar <Divakar.R@cerner.com>
2020-03-01 21:24:01 +0530

    added dynamic hats route

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title , imageUrl , size}) => (
+const MenuItem = ({ title , imageUrl , size , history , linkUrl, match}) => (
     
     <div
+        onClick = {() => history.push(`${match.url}${linkUrl}`)}
         className={`${size} menu-item`}>
         <div className='background-image'
         style={{
             backgroundImage: `url(${imageUrl})` 
         }}
         
-        ></div>
+        />
 
     <div className='content'>
         <h1 className='title'>
@@ -23,4 +26,4 @@ const MenuItem = ({ title , imageUrl , size}) => (
     </div>
 </div>
 );
-export default MenuItem;
+export default withRouter( MenuItem);
